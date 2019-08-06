21fc3a516c7e009a937b2fb5b9af5fbfc9770ab0
Jonathan Luk <jluk87@gmail.com>
2019-08-06 22:57:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  const driversMatched = [];
+  for (const driver of drivers) {
+    if driver == name {
+      driversMatched.push(driver);
+    }
+  }
+  return driversMatched;
+}
